6174b4c9209ddd739b4327d6badc9a5299e0b53b
dietrili <dietrich.lisaivi@gmail.com>
2022-08-23 11:39:04 +0200

    commit class auto

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,21 @@
 public class Auto {
+    public static int AnzahlReifen = 4;  //Statisches Feld
+    private String Lackfarbe;            // Feld
+    public int AnzahlTueren;
+
+    public static int getAnzahlReifen(){        //static methode
+        return AnzahlReifen;
+    }
+    public void setLackfarbe(String Farbe){     //methode
+        Lackfarbe = Farbe;
+    }
+
+    Auto(String Lackfarbe, int AnzahlTueren){
+        this.Lackfarbe = Lackfarbe;
+        this.AnzahlTueren = AnzahlTueren;
+
+    }
+    public static void main(String[] args){
+        Auto auto1 = new Auto("Rot",5);
+    }
 }
